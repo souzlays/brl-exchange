57e0cbdf8553df124a2fb1d7f85428be433cde91
souzlays <souzalaayss@gmail.com>
2025-11-05 23:14:17 -0300

    layout da aplicação finalizado e responsivo

--- src/app/components/search-form/search-form.component.ts
+++ src/app/components/search-form/search-form.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule, NgModel } from '@angular/forms';
 
@@ -6,18 +7,27 @@ import { FormsModule, NgModel } from '@angular/forms';
   selector: 'app-search-form',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    CommonModule
   ],
   templateUrl: './search-form.component.html',
   styleUrls: ['./search-form.component.css'],
 })
 export class SearchFormComponent {
   currencyCode = '';
+  showError = false;
   @Output() search = new EventEmitter<string>();
 
   onSubmit() {
-    if (this.currencyCode.trim()) {
-      this.search.emit(this.currencyCode.toUpperCase());
+    const code = this.currencyCode.trim().toUpperCase();
+
+    const isValid = /^[A-Z]{3}$/.test(code);
+
+    if (isValid) {
+      this.showError = false;
+      this.search.emit(code);
+    } else {
+      this.showError = true;
     }
   }
 }
